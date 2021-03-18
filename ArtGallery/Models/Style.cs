3e81ca6ce0b20d9b99f82e30442e6bbdd33fdb16
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-18 13:02:02 -0700

    Add JoinEntities HashSet, using directives, auto implemented properties with enum for period/medium
    Co-authored-by: MorganJBradford <MorganJBradford@users.noreply.github.com>

--- ArtGallery/Models/Style.cs
+++ ArtGallery/Models/Style.cs
@@ -1,7 +1,53 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
 namespace ArtGallery.Models
 {
   public class Style
   {
-    // properties, constructors, methods, etc. go here
+    public Style()
+    {
+      this.JoinEntities = new HashSet<Gallery>();
+    }
+
+    public int StyleId { get; set; }
+    public Period Period { get; set; } 
+    public Medium Medium { get; set; }
+
+    public virtual ICollection<Gallery> JoinEntities { get; }
+  }
+
+  public enum Period
+  {
+    [Description("Ancient Classical Period")]
+    AncientClassicalPeriod,
+    [Description("Medieval Period")]
+    Medieval,
+    [Description("Renaissance Period")]
+    RenaissancePeriod,
+    [Description("Neoclassicism Period")]
+    NeoclassicismPeriod,
+    [Description("Romanticism Period")]
+    RomanticismPeriod,
+    [Description("Modern Period")]
+    ModernPeriod,
+    [Description("Contemporary Period")]
+    ContemporaryPeriod
+  }
+
+  public enum Medium {
+    [Description("Painting")]
+    Painting,
+    [Description("Drawing")]
+    Drawing,
+    [Description("Printing")]
+    Printing,
+    [Description("Textiles")]
+    Textiles,
+    [Description("Photography/Film/Technology")]
+    PhotographyFilmTechnology,
+    [Description("Sculpture")]
+    Sculpture
+
   }
 }
