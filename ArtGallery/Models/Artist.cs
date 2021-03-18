5b5b57f4e7e789b8a19ac76b9c25861c15d8a10a
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-18 13:05:37 -0700

    Add JoinEntities HashSet, using directives, auto implemented properties with validation
    Co-authored-by: MorganJBradford <MorganJBradford@users.noreply.github.com>

--- ArtGallery/Models/Artist.cs
+++ ArtGallery/Models/Artist.cs
@@ -1,10 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace ArtGallery.Models
 {
   public class Artist
   {
     public Artist()
     {
-      this.JoinEntities = new HashSet<
+      this.JoinEntities = new HashSet<Gallery>();
     }
+
+    public int ArtistId { get; set;}
+    [Required]
+    [Display(Name="Name")]
+    public string ArtistName { get; set; }
+    [Required]
+    public string Birthplace { get; set; }
+    [Required]
+    [Range(0,120)]
+    public int Age { get; set; }
+    [Required]
+    public string Bio { get; set; }
+    public virtual ICollection<Gallery> JoinEntities { get; set; }
   }
 }
