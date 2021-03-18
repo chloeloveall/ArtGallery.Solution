851382ea657ead4d4662a0ed85a9cdc8ef5e80c8
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-18 13:17:26 -0700

    Add DbContext/DbSet
    Co-authored-by: MorganJBradford <MorganJBradford@users.noreply.github.com>

--- ArtGallery/Models/ArtGalleryContext.cs
+++ ArtGallery/Models/ArtGalleryContext.cs
@@ -4,10 +4,18 @@ namespace ArtGallery.Models
 {
   public class ArtGalleryContext : DbContext
   {
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Style> Styles { get; set; }
+    public DbSet<Gallery> Gallery { get; set; }
+    public ArtGalleryContext(DbContextOptions options) : base(options) { }
 
 
-    // public ArtGalleryContext(DbContextOptions options) : base(options) { }
+    // Add virtual to utilize lazy loading
+    // public virtual DbSet<Artist> Artists { get; set; }
+    // public virtual DbSet<Style> Styles { get; set; }
+    // public virtual DbSet<Gallery> Gallery { get; set; }
 
+    // Adds ability to use lazy loading
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     // {
     //   optionsBuilder.UseLazyLoadingProxies();
